fe249589d77e36176101567913fd521919cd7cf2
Richard Neumann <mail@richard-neumann.de>
2023-05-20 23:13:39 +0200

    Allow whitespace-separated input

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,21 @@ use std::io::stdin;
 
 fn main() {
     for range in Ranges::from(read_integers()) {
-        print!("{{{}..{}}} ", range.start(), range.end());
+        println!("{{{}..{}}} ", range.start(), range.end());
     }
-
-    println!();
 }
 
 fn read_integers() -> impl Iterator<Item = i64> {
     stdin()
         .lines()
         .take_while(|line| line.is_ok())
-        .map(|line| line.unwrap().parse::<i64>())
+        .map(|line| line.unwrap())
+        .flat_map(|line| {
+            line.split_ascii_whitespace()
+                .map(str::to_owned)
+                .collect::<Vec<_>>()
+        })
+        .map(|line| line.parse::<i64>())
         .take_while(|result| result.is_ok())
         .map(|result| result.unwrap())
 }
