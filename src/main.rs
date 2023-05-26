a98962784557202552e815e12ffbd40475a3c747
Richard Neumann <mail@richard-neumann.de>
2023-05-26 02:46:03 +0200

    Make helper functions generic

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use ranges::Ranges;
+use std::fmt::{Debug, Display};
 use std::io::stdin;
 use std::ops::RangeInclusive;
+use std::str::FromStr;
 
 fn main() {
     println!(
         "{}",
-        read_integers()
+        read_integers::<i64>()
             .ranges()
             .map(range_to_bash_literal)
             .collect::<Vec<_>>()
@@ -13,7 +15,11 @@ fn main() {
     )
 }
 
-fn read_integers() -> impl Iterator<Item = i64> {
+fn read_integers<T>() -> impl Iterator<Item = T>
+where
+    T: FromStr,
+    T::Err: Debug,
+{
     stdin()
         .lines()
         .take_while(|line| line.is_ok())
@@ -23,12 +29,15 @@ fn read_integers() -> impl Iterator<Item = i64> {
                 .map(str::to_owned)
                 .collect::<Vec<_>>()
         })
-        .map(|number| number.parse::<i64>())
+        .map(|number| number.parse::<T>())
         .take_while(|result| result.is_ok())
         .map(|result| result.unwrap())
 }
 
-fn range_to_bash_literal(range: RangeInclusive<i64>) -> String {
+fn range_to_bash_literal<T>(range: RangeInclusive<T>) -> String
+where
+    T: Display + PartialEq,
+{
     if range.start() == range.end() {
         range.start().to_string()
     } else {
